11380f71246b1f529fa4c538d765215def183c7b
Louise Helene Hamle <louielouie5@msn.com>
2021-09-28 04:47:29 +0200

    Renaming solution and projects again, now to "BiasApp" and implementing architecture. Also adding two empty classes "Card.cs" and "Storage.cs".

---
+++ BiasApp.UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BiasApp.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new BiasApp.App());
+        }
+    }
+}
---
+++ BiasApp/Storage/Storage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BiasApp.Storage
+{
+    public class Storage
+    {
+
+    }
+}
